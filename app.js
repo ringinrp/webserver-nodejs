ddc1664ddfdf9c4a1d6214d6685423b7ca783219
ringinrp <ringinrp21@gmail.com>
2023-09-12 07:42:22 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const  http = require('http');
+const port = 3000;
 
 http.createServer((req, res)=>{
     res.write('Hello World');
     res.end();
-}).listen(3000, ()=>{
-    console.log('Server is listening on port 3000');
+}).listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`);
 });
